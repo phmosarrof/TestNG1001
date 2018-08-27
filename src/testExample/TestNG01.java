8f44dd2a48de60f6082f765acf8c7184ee7d273c
Mosarrof Hossain <phmosharaf@gmail.com>
2018-08-27 16:59:04 -0400

    TestNG

---
+++ src/testExample/TestNG01.java
@@ -0,0 +1,29 @@
+package testExample;
+
+import org.testng.annotations.Test;
+
+public class TestNG01 {
+
+	//public static void main(String[] args) {
+	@Test
+	public void testCase01() {
+		
+		System.out.println("\nThe Test is run by TestNG Framework - Test01.");
+
+	}
+
+	@Test
+	public void testCase02() {
+		
+		System.out.println("\nThe Test is run by TestNG Framework - Test02.");
+
+	}
+	
+	@Test
+	public void testCase03() {
+		
+		System.out.println("\nThe Test is run by TestNG Framework - Test03.\n");
+
+	}
+}
+
---
+++ src/testExample/TestNG02.java
@@ -0,0 +1,18 @@
+package testExample;
+
+import org.testng.annotations.Test;
+
+public class TestNG02 {
+	public static void main (String [] args){
+		
+	}
+	@Test
+	public void Test04() {
+		System.out.println("System.out.println(\"\\nThe Test is run by TestNG Framework- Test04");
+	}
+	@Test
+	public void Test05() {
+		System.out.println("System.out.println(\"\\nThe Test is run by TestNG Framework- Test05");
+	}
+}
+
